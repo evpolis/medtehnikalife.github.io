4e4a93db4e9a449462bffa47ef432e78436dcdfa
evpolis <36083315+evpolis@users.noreply.github.com>
2018-10-10 14:47:30 +0300

    medTeh Commit

--- js/custom.js
+++ js/custom.js
@@ -21,6 +21,78 @@ $(document).ready(function() {
 				}
 			});
 		})
+	}); 
+
+
+	$('.js-button-campaign').click(function() { 
+	
+		$('.js-overlay-campaign').fadeIn();
+		$('.js-overlay-campaign').addClass('disabled');
 	});
-			
+
+
+	$(document).mouseup(function (e) { 
+		var popup = $('.js-popup-campaign');
+		if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+			$('.js-overlay-campaign').fadeOut();
+		}
+	});
+
+	 $( function() {
+	    $( "#slider-range" ).slider({
+	      range: true,
+	      min: 721,
+	      max: 1056,
+	      values: [ 721, 1056 ],
+	      slide: function( event, ui ) {
+	        $( "#amount" ).val(ui.values[ 0 ]);
+	        $( "#amount_1" ).val(ui.values[ 1 ]);
+	      }
+	    });
+	    $( "#amount" ).val($( "#slider-range" ).slider( "values", 0 ) );
+	    $( "#amount_1" ).val($( "#slider-range" ).slider( "values", 1 ) );
+	});
+
+	$("input#amount").change(function(){
+	  	var value1=$("input#amount").val();
+	  	var value2=$("input#amount_1").val();
+	      if(parseInt(value1) > parseInt(value2)){
+	  		value1 = value2;
+	  		$("input#amount").val(value1);
+	  	}
+	  	$("#slider-range").slider("values",0,value1);	
+	});
+	      
+	  
+	$("input#amount_1").change(function(){
+		var value1=$("input#amount").val();
+	  	var value2=$("input#amount_1").val();
+
+	  	if(parseInt(value1) > parseInt(value2)){
+	  		value2 = value1;
+	  		$("input#amount_1").val(value2);
+	  	}
+	  	$("#slider-range").slider("values",1,value2);
+	});
+
+	jQuery('#amount, #amount_1').keypress(function(event){
+		var key, keyChar;
+		if(!event) var event = window.event;
+		
+		if (event.keyCode) key = event.keyCode;
+		else if(event.which) key = event.which;
+	
+		if(key==null || key==0 || key==8 || key==13 || key==9 || key==46 || key==37 || key==39 ) return true;
+		keyChar=String.fromCharCode(key);
+		
+		if(!/\d/.test(keyChar))	return false;
+		
+	});
+
+	// $('.popup-bottom').slick({
+	// 	infinite: true,
+ //  		slidesToShow: 3,
+ //  		slidesToScroll: 3,
+	// });
+		
 });
